7b365b1abbd63a4334dc2531e342e9ea8f0ab595
pjsinco <psinco@gmail.com>
2017-04-09 08:49:03 -0500

    .splash__block positioned OK on small screens

--- template-parts/content-splash.php
+++ template-parts/content-splash.php
@@ -10,18 +10,20 @@ $splash_header = get_field( 'omed_splash_header', $post->ID );
 $splash_body = get_field( 'omed_splash_body', $post->ID );
 
 ?>
-<section class="splash splash--<?php if ( is_page()): global $post; if ( isset( $post ) ): echo $post->post_name; endif; endif; ?> <?php if ( is_front_page() ): echo 'splash--nomargin'; endif; ?>">
-  <div class="splash__border--top"></div>
-  <div class="wrap">
-    <div class="splash__block container-fluid" style="visibility: hidden;">
-      <div class="splash__header">
-        <?php if ($splash_header): echo $splash_header; endif; ?>
-      </div>
-      <div class="splash__body">
-        <?php if ($splash_body): echo $splash_body; endif; ?>
-      </div>
-    </div> <!-- .splash__block -->
-  </div> <!-- .container-fluid -->
-  <div class="splash__border--bottom"></div>
+<section class="splash--<?php if ( is_page()): global $post; if ( isset( $post ) ): echo $post->post_name; endif; endif; ?> <?php // if ( is_front_page() ): echo 'splash--nomargin'; endif; ?> pageblock">
+  <div class="splash__content">
+    <div class="splash__border--top"></div>
+    <div class="wrap">
+      <div class="splash__block container-fluid">
+        <div class="splash__header">
+          <?php if ($splash_header): echo $splash_header; endif; ?>
+        </div>
+        <div class="splash__body">
+          <?php if ($splash_body): echo $splash_body; endif; ?>
+        </div>
+      </div> <!-- .splash__block -->
+    </div> <!-- .container-fluid -->
+    <div class="splash__border--bottom"></div>
+  </div> <!-- .splash__content -->
 </section> <!-- .splash -->
 
