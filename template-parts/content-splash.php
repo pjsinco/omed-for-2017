d85c11cf0a8afc828a0bf3d153740545b97a5541
pjsinco <psinco@gmail.com>
2017-04-09 15:29:26 -0500

    Get splash looking OK across all screens

--- template-parts/content-splash.php
+++ template-parts/content-splash.php
@@ -11,19 +11,17 @@ $splash_body = get_field( 'omed_splash_body', $post->ID );
 
 ?>
 <section class="splash--<?php if ( is_page()): global $post; if ( isset( $post ) ): echo $post->post_name; endif; endif; ?> <?php // if ( is_front_page() ): echo 'splash--nomargin'; endif; ?> pageblock">
-  <div class="splash__content">
-    <div class="splash__border--top"></div>
-    <div class="wrap">
-      <div class="splash__block container-fluid">
-        <h1 class="splash__header">
-          <?php if ($splash_header): echo $splash_header; endif; ?>
-        </h1>
-        <p class="splash__body">
-          <?php if ($splash_body): echo $splash_body; endif; ?>
-        </p>
-      </div> <!-- .splash__block -->
-    </div> <!-- .container-fluid -->
-    <div class="splash__border--bottom"></div>
+  <div class="splash__border--top"></div>
+  <div class="splash__content wrap">
+    <div class="splash__block">
+      <h1 class="splash__header">
+        <?php if ($splash_header): echo $splash_header; endif; ?>
+      </h1>
+      <p class="splash__body">
+        <?php if ($splash_body): echo $splash_body; endif; ?>
+      </p>
+    </div> <!-- .splash__block -->
   </div> <!-- .splash__content -->
+  <div class="splash__border--bottom"></div>
 </section> <!-- .splash -->
 
