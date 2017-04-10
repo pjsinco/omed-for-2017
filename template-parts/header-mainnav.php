b81c09ac5e4e495369fdcda389dcad562c8310db
pjsinco <psinco@gmail.com>
2017-04-10 16:25:05 -0500

    Intro block is roughly positioned on big screens

--- template-parts/header-mainnav.php
+++ template-parts/header-mainnav.php
@@ -23,7 +23,7 @@
       <li>
         <a href="http://osteopathic.org">
           <svg class="icon icon-omed-logo-plain" preserveAspectRatio="xMidYMid meet" width="70" height="33">
-            <use xlink:href="<?php echo get_template_directory_uri(); ?>/public/defs.svg?version=<?php echo filemtime(get_template_directory() . '/public/defs.svg'); ?>#aoaLogo" />
+            <use xlink:href="<?php echo get_template_directory_uri(); ?>/public/defs.svg?version=<?php echo filemtime(get_template_directory() . '/public/defs.svg'); ?>#aoaLogoNoFill" />
           </svg>
         </a>
       </li>
