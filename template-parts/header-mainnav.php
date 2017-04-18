99adf2ebb337c5bbe7ae47f10bb1d05fc316e3c7
pjsinco <psinco@gmail.com>
2017-04-18 11:46:35 -0500

    Get secondary nav looking decent

--- template-parts/header-mainnav.php
+++ template-parts/header-mainnav.php
@@ -31,13 +31,14 @@
     
   </nav> <!-- .primary-nav -->
 
-<!--   <nav class="secondary-nav"> -->
-<!--     <ul> -->
-<!--       <li><a href="#">FAQs</a></li> -->
-<!--       <li><a href="#">Contact Us</a></li> -->
-<!--       <li><a href="#">For Exhibitors</a></li> -->
-<!--       <li><a href="#">Marketing</a></li> -->
-<!--     </ul> -->
+  <nav class="secondary-nav">
+    <ul>
+      <li class="alt"><a href="#">Register</a></li>
+      <li><a href="#">FAQs</a></li>
+      <li><a href="#">Contact Us</a></li>
+      <li><a href="#">For Exhibitors</a></li>
+      <li><a href="#">Marketing Toolkit</a></li>
+    </ul>
     
   </nav>
   
