279263e0da074ad99bd5e6cc7d7bb39de1df4891
pjsinco <psinco@gmail.com>
2017-04-21 13:07:50 -0500

    Give width, height attrs to svgstore task

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function(grunt) {
         options: {
           svg: {
             xmlns: 'http://www.w3.org/2000/svg',
-            display: 'none',
+            width: '0',
+            height: '0',
           },
         },
         files: {
