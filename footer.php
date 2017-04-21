dd31551f8605a56b870a67d83596e6ab367edc2c
pjsinco <psinco@gmail.com>
2017-04-21 03:29:28 -0500

    Position 3 columns in footer

--- footer.php
+++ footer.php
@@ -13,10 +13,28 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="footer">
+		<div class="footer__body container-fluid wrap">
+      <div class="footer__col--half">
+        <a href="http://osteopathic.org">
+          <svg class="icon icon-aoa-logo" preserveAspectRatio="xMidYMid meet" width="70" height="33">
+            <use xlink:href="<?php echo get_template_directory_uri(); ?>/public/defs.svg?version=<?php echo filemtime(get_template_directory() . '/public/defs.svg'); ?>#aoaLogoNoFill" />
+          </svg>
+        </a>
+      </div>
+      
+      <div class="footer__col--quarter">
+hi from quarter        
+      </div>
+
+      <div class="footer__col--quarter">
+hi from quarter        
+      </div>
+		</div><!-- .wrap -->
+    <div class="copyright">
+      
+    </div>
+	</footer>
 </div><!-- #page -->
 
 <?php echo file_get_contents( get_template_directory_uri() . '/public/defs.svg'); ?>
