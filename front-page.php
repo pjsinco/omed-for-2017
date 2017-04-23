8a87175fc73bd27ff070c1aa25836a5ad0ebbd17
pjsinco <psinco@gmail.com>
2017-04-23 14:33:06 -0500

    Change highlightable image

--- front-page.php
+++ front-page.php
@@ -59,7 +59,7 @@
       <div class="highlightable__block wrap">
         <div class="highlightable__body">
           <div class="highlightable__imagecontainer">
-            <img class="highlightable__image" src="wp-content/themes/omed-for-2017/public/images/omed-2016-04.jpg" alt="OMT session">
+            <img class="highlightable__image" src="wp-content/uploads/2017/04/omed-201.jpg" alt="OMT session">
           </div> <!-- .highlightable__imagecontainer -->
           <div class="highlightable__text">
             <h5 class="highlightable__header">Maecenas sed diam eget risus varius blandit</h5>
