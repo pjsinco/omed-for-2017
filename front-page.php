8f8c321053f02eec73f13dd00214a4960b8e34c1
pjsinco <psinco@gmail.com>
2017-06-12 11:02:15 -0500

    Make highlightable shortcode

--- front-page.php
+++ front-page.php
@@ -53,25 +53,6 @@
 <!--   <div class="fragment-1"></div> -->
 <!-- </section> -->
 
-<section class="highlightable container-fluid pageblock"  >
-  <div class="highlightable__block wrap">
-    <div class="highlightable__body">
-      <div class="highlightable__imagecontainer">
-      <?php if ( is_dev_env() ): ?>
-        <img class="highlightable__image" src="wp-content/uploads/2017/04/omed-861.jpg" alt="OMT session">
-      <?php else: ?>
-        <img class="highlightable__image" src="wp-content/uploads/2016/11/omed-861.jpg" alt="OMT session">
-      <?php endif ?>
-      </div> <!-- .highlightable__imagecontainer -->
-      <div class="highlightable__text">
-        <h5 class="highlightable__header">Q: What is the Experience Zone?</h5>
-        <h4 class="highlightable__blurb">The Experience Zone is the center of the action at OMED&mdash;a hub for gathering and networking and a place to discover technologies.</h4>
-        <a href="/omed-2017-faqs/" class="highlightable__button btn btn--sm btn--reverse">See more FAQs</a>
-      </div> <!-- .highlightable__text -->
-    </div> <!-- .highlightable__body -->
-  </div> <!-- .highlightable__block -->
-</section>
-
 <!-- <section class="fragment__container--left rellax" data-rellax-speed="-4"> -->
 <!--   <div class="fragment-2"></div> -->
 <!-- </section> -->
