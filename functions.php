2554c0b6a1d13cb30d578b134dd91f08c1d38181
pjsinco <psinco@gmail.com>
2017-06-26 10:21:19 -0500

    Add Worldata pixel code

--- functions.php
+++ functions.php
@@ -749,6 +749,31 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 }
 add_action( 'just_opened_body_tag' , 'omed_add_google_tag_manager_body_code' );
 
+function omed_add_worldata_pixel() {
+  if ( is_dev_env() || is_admin() ) {
+    return;
+  }
+?>
+<script>
+var versaTag = {};
+versaTag.id = "7475";
+versaTag.sync = 0;
+versaTag.dispType = "js";
+versaTag.ptcl = "HTTPS";
+versaTag.bsUrl = "bs.serving-sys.com/BurstingPipe";
+versaTag.activityParams = { "Session":"" };
+versaTag.retargetParams = {};
+versaTag.dynamicRetargetParams = {};
+versaTag.conditionalParams = {};
+</script>
+<script id="ebOneTagUrlId" src="https://secure-ds.serving-sys.com/SemiCachedScripts/ebOneTag.js"></script>
+<noscript>
+<iframe src="https://bs.serving-sys.com/BurstingPipe?cn=ot&amp;onetagid=7475&amp;ns=1&amp;activityValues=$$Session=[Session]$$&amp;retargetingValues=$$$$&amp;dynamicRetargetingValues=$$$$&amp;acp=$$$$&amp;" style="display:none;width:0px;height:0px"></iframe>
+</noscript>
+<?php
+}
+add_action('just_opened_body_tag' , 'omed_add_worldata_pixel');
+
 function omed_add_custom_ninja_form_class ( $form_class, $form_id ) 
 {
 
