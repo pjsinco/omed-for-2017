6e780792e56442f02341704a610b6142cabbc046
pjsinco <psinco@gmail.com>
2017-11-16 10:16:56 -0600

    Add OMED 2018 logo to mainnav

--- template-parts/header-mainnav.php
+++ template-parts/header-mainnav.php
@@ -3,7 +3,7 @@
   <div class="logo">
     <a href="<?php echo esc_html( home_url('/') ); ?>">
       <svg class="icon icon-omed-logo-plain" preserveAspectRatio="xMidYMid meet" width="200" height="35">
-        <use xlink:href="<?php echo get_template_directory_uri(); ?>/public/defs.svg?version=<?php echo filemtime(get_template_directory() . '/public/defs.svg'); ?>#omedLogoPlain" />
+        <use xlink:href="<?php echo get_template_directory_uri(); ?>/public/defs.svg?version=<?php echo filemtime(get_template_directory() . '/public/defs.svg'); ?>#omed2018LogoPlain" />
       </svg>
     </a>
   </div>
